7cb9643ee6a1d1971873a1634bf0e98852075134
Sophea Phal <pheazha310@gmail.com>
2026-03-31 07:53:52 +0700

    Fix welcome view error

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'app' => config('app.name'),
+        'status' => 'ok',
+        'message' => 'Chomnuoy backend is running.',
+    ]);
 });
