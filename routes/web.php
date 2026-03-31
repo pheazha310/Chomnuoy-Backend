ed1351d56b8461a030f7246e840ed5a1467507b5
Sophea Phal <pheazha310@gmail.com>
2026-03-31 08:15:13 +0700

    Fix error

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/favicon.ico', function () {
-    return response()->noContent();
-});
-
 Route::get('/', function () {
-    return response()->json([
-        'app' => config('app.name'),
-        'status' => 'ok',
-        'message' => 'Chomnuoy backend is running.',
-    ]);
+    return view('welcome');
 });
